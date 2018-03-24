0a0a37c8d17b0a95f7bb2ff8fa7ee3284a8c9207
pescadl <pescadlee@gmail.com>
2018-03-24 16:37:14 -0500

    everything seems to work

--- lab2/Graph.java
+++ lab2/Graph.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.lang.Math;
 /*
  * Name: Dayoung Lee
  * EID: dl29923
@@ -8,15 +8,12 @@ import java.util.PriorityQueue;
 
 public class Graph implements Program2{
 	// Edge is the class to represent an edge between two nodes
-	// node is the destination node this edge connected to
 	// time is the travel time of this edge
 	// capacity is the capacity of this edge
 	private class Edge{
-		public int node;
 		public int time;
 		public int capacity;
-		public Edge(int n, int t, int c){
-			node = n;
+		public Edge(int t, int c){
 			time = t;
 			capacity = c;
 		}
@@ -28,22 +25,12 @@ public class Graph implements Program2{
 	// graph is an adjacency matrix
 	private Edge[][] graph;
 
-	// adjacencyLists is a collection of adjacency lists
-	// only used to for getNeighbors function because example_output file used adjacency lists
-	// and I was too lazy to actually read the numbers one by one with my adjacency Matrix
-	// no longer needed, I think
-	// private ArrayList<LinkedList<Edge>> adjacencyLists;
-
 	// This function is the constructor of the Graph. Do not change the parameters
 	// of this function.
 	// initialize the adjacency matrix and adjacency lists
 	public Graph(int x) {
 		V = x;
 		graph = new Edge[V][V];
-		// adjacencyLists = new ArrayList<LinkedList<Edge>>();
-		// for(int i=0; i<V; i++) {
-		// 	adjacencyLists.add(new LinkedList<Edge>());
-		// }
 	}
     
 	// This function is called by Driver. The input is an edge in the graph.
@@ -51,10 +38,7 @@ public class Graph implements Program2{
 	// Do not change its parameters or return type.
 	// Hint: Here is the place to build the graph with the data structure you defined.
 	public void inputEdge(int port1, int port2, int time, int capacity) {
-		graph[port1][port2] = new Edge(port2, time, capacity);
-		graph[port2][port1] = new Edge(port1, time, capacity);
-		// adjacencyLists.get(port1).add(new Edge(port2, time, capacity));
-		// adjacencyLists.get(port2).add(new Edge(port1, time, capacity));
+		graph[port1][port2] = graph[port2][port1] = new Edge(time, capacity);
 		return;
 	}
 
@@ -63,27 +47,27 @@ public class Graph implements Program2{
 	// from source port to destination port
 	// Do not change its parameters or return type.
 	public int findTimeOptimalPath(int sourcePort, int destPort) {
-		int[] dist = new int[V];
+		int[] time = new int[V];
 
-		for(int i=0; i<V; i++) { dist[i] = Integer.MAX_VALUE; }
-		dist[sourcePort] = 0;
-
-		PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
+		for(int i=0; i<V; i++) { time[i] = Integer.MAX_VALUE; }
+		time[sourcePort] = 0;
+		
+		ArrayList<Integer> queue = new ArrayList<Integer>();
 		queue.add(sourcePort);
 
 		while(!queue.isEmpty()) {
-			int u = queue.poll();
+			int u = queue.remove(0);
 			ArrayList<Integer> neighbors = getNeighbors(u);
 
 			for(int v : neighbors) {
-				if(dist[v] > dist[u] + graph[u][v].time) {
-					dist[v] = dist[u] + graph[u][v].time;
+				if(time[v] > time[u] + graph[u][v].time) {
+					time[v] = time[u] + graph[u][v].time;
 					queue.add(v);
 				}
 			}
 		}
 
-		return dist[destPort];
+		return time[destPort];
 	}
 
 	// This function is the solution for the Widest Path problem.
@@ -91,7 +75,27 @@ public class Graph implements Program2{
 	// source port to destination port 
 	// Do not change its parameters or return type.
 	public int findCapOptimalPath(int sourcePort, int destPort) {
-		return -1;
+		int[] cap = new int[V];
+		
+		for(int i=0; i<V; i++) { cap[i] = 0; }
+		cap[sourcePort] = Integer.MAX_VALUE;
+	
+		ArrayList<Integer> queue = new ArrayList<Integer>();
+		queue.add(sourcePort);
+
+		while(!queue.isEmpty()) {
+			int u = queue.remove(0);
+			ArrayList<Integer> neighbors = getNeighbors(u);
+
+			for(int v : neighbors) {
+				if(Math.min(cap[u], graph[u][v].capacity) > cap[v]) {
+					cap[v] = Math.min(cap[u], graph[u][v].capacity);
+					queue.add(v);
+				}
+			}
+		}
+
+		return cap[destPort];
 	}
 
 	// This function returns the neighboring ports of node.
@@ -104,11 +108,7 @@ public class Graph implements Program2{
 				edges.add(dest);
 			}
 		}
-
-		// for(Edge e : adjacencyLists.get(node)) {
-		// 	edges.add(e.node);
-		// }
-
+		
 		return edges;
 	}
 
