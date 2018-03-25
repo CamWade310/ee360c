4f3230a8f479b3491f8041e42fbe702df9d1f7a4
pescadl <pescadlee@gmail.com>
2018-03-25 15:12:11 -0500

    fixed Graph code for time and capacity optimal so I actually use the min-extract and max-extract for the queue ... finished lab report ... now just need to make sure everything works on LRC so I can turn it in

--- lab2/Graph.java
+++ lab2/Graph.java
@@ -48,26 +48,37 @@ public class Graph implements Program2{
 	// Do not change its parameters or return type.
 	public int findTimeOptimalPath(int sourcePort, int destPort) {
 		int[] time = new int[V];
-
-		for(int i=0; i<V; i++) { time[i] = Integer.MAX_VALUE; }
+		for(int node=0; node<V; node++) { time[node] = Integer.MAX_VALUE; }
 		time[sourcePort] = 0;
 		
 		ArrayList<Integer> queue = new ArrayList<Integer>();
-		queue.add(sourcePort);
+		for(int node=0; node<V; node++) { queue.add(node); }
 
 		while(!queue.isEmpty()) {
-			int u = queue.remove(0);
+			int u = queue.remove(min(queue, time));
 			ArrayList<Integer> neighbors = getNeighbors(u);
 
 			for(int v : neighbors) {
 				if(time[v] > time[u] + graph[u][v].time) {
 					time[v] = time[u] + graph[u][v].time;
-					queue.add(v);
 				}
 			}
 		}
 
 		return time[destPort];
+	}
+
+	private int min(ArrayList<Integer> queue, int[] time) {
+		int min_time = Integer.MAX_VALUE;
+		int min_index = -1;
+		for(int node=0; node<queue.size(); node++) {
+			if(min_time >= time[queue.get(node)]) {
+				min_time = time[queue.get(node)];
+				min_index = node;
+			}
+		}
+
+		return min_index;
 	}
 
 	// This function is the solution for the Widest Path problem.
@@ -76,21 +87,19 @@ public class Graph implements Program2{
 	// Do not change its parameters or return type.
 	public int findCapOptimalPath(int sourcePort, int destPort) {
 		int[] cap = new int[V];
-		
-		for(int i=0; i<V; i++) { cap[i] = 0; }
+		for(int node=0; node<V; node++) { cap[node] = 0; }
 		cap[sourcePort] = Integer.MAX_VALUE;
 	
 		ArrayList<Integer> queue = new ArrayList<Integer>();
-		queue.add(sourcePort);
+		for(int node=0; node<V; node++) { queue.add(node); }
 
 		while(!queue.isEmpty()) {
-			int u = queue.remove(0);
+			int u = queue.remove(max(queue, cap));
 			ArrayList<Integer> neighbors = getNeighbors(u);
 
 			for(int v : neighbors) {
 				if(Math.min(cap[u], graph[u][v].capacity) > cap[v]) {
 					cap[v] = Math.min(cap[u], graph[u][v].capacity);
-					queue.add(v);
 				}
 			}
 		}
@@ -98,6 +107,18 @@ public class Graph implements Program2{
 		return cap[destPort];
 	}
 
+	private int max(ArrayList<Integer> queue, int[] cap) {
+		int max_cap = -1;
+		int max_index = -1;
+		for(int node=0; node<queue.size(); node++) {
+			if(max_cap <= cap[queue.get(node)]) {
+				max_cap = cap[queue.get(node)];
+				max_index = node;
+			}
+		}
+
+		return max_index;
+	}
 	// This function returns the neighboring ports of node.
 	// This function is used to test if you have contructed the graph correct.
 	public ArrayList<Integer> getNeighbors(int node) {
