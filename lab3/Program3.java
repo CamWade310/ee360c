1a3855620d5729f76fb529a15df747932b966e93
dayoung <pescadlee@gmail.com>
2018-04-14 14:31:34 -0500

    started lab3

---
+++ lab3/Program3.java
@@ -0,0 +1,56 @@
+
+//Name: (Fill this in)
+//EID: (Fill this in)
+
+
+public class Program3 {
+
+    EconomyCalculator calculator;
+    VibraniumOreScenario vibraniumScenario;    
+
+    public Program3() {
+        this.calculator = null;
+        this.vibraniumScenario = null;
+    }
+
+    /*
+     * This method is used in lieu of a required constructor signature to initialize
+     * your Program3. After calling a default (no-parameter) constructor, we
+     * will use this method to initialize your Program3 for Part 1.
+     */
+    public void initialize(EconomyCalculator ec) {
+        this.calculator = ec;
+    }
+
+    /*
+     * This method is used in lieu of a required constructor signature to initialize
+     * your Program3. After calling a default (no-parameter) constructor, we
+     * will use this method to initialize your Program3 for Part 2.
+     */
+    public void initialize(VibraniumOreScenario vs) {
+        this.vibraniumScenario = vs;
+    }
+
+    /*
+     * This method returns an integer that is maximum possible gain in the Wakandan economy
+     * given a certain amount of Vibranium
+     */
+    //TODO: Complete this function
+    public int computeGain() {
+
+        return 0;
+    }
+
+    /*
+     * This method returns an integer that is the maximum possible dollar value that a thief 
+     * could steal given the weight and volume capacity of his/her bag by using the 
+     * VibraniumOreScenario instance.
+     */
+     //TODO: Complete this method
+     public int computeLoss() {
+        
+        return 0;
+     }
+}
+
+
